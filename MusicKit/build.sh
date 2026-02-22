e237dccbd9410fc787db14468bcd86d480dd7238
Gregory John Casamento <greg.casamento@gmail.com>
2026-02-22 00:25:14 -0500

    Add install.sh update GNUmakefiles

--- MusicKit/build.sh
+++ MusicKit/build.sh
@@ -2,26 +2,32 @@
 
 echo "### Build MusicKit..."
 
+. ${GNUSTEP_MAKEFILES}/GNUstep.sh
+
 # Build DSP Native
 cd Frameworks
 cd MKDSP_Native
 make debug=yes
+su -c ./install.sh
 
 # Build portaudio MIDI
 cd ..
 cd PlatformDependent
 cd MKPerformSndMIDI_portaudio
 make debug=yes
+su -c ./install.sh
 
 # Build SndKit
 cd ../..
 cd SndKit
 make debug=yes
+su -c ./install.sh
 
 # Build MusicKit
 cd ../
 cd MusicKit
 make debug=yes
+su -c ./install.sh
 
 echo "### Done"
 
