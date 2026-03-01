e8a84490f99eef1a138afa0f08b100ab52357707
Gregory John Casamento <greg.casamento@gmail.com>
2026-03-01 07:41:27 -0500

    Use sudo

--- MusicKit/build.sh
+++ MusicKit/build.sh
@@ -8,26 +8,26 @@ echo "### Build MusicKit..."
 cd Frameworks
 cd MKDSP_Native
 make debug=yes
-su -c ./install.sh
+sudo ./install.sh
 
 # Build portaudio MIDI
 cd ..
 cd PlatformDependent
 cd MKPerformSndMIDI_portaudio
 make debug=yes
-su -c ./install.sh
+sudo ./install.sh
 
 # Build SndKit
 cd ../..
 cd SndKit
 make debug=yes
-su -c ./install.sh
+sudo ./install.sh
 
 # Build MusicKit
 cd ../
 cd MusicKit
 make debug=yes
-su -c ./install.sh
+sudo ./install.sh
 
 echo "### Done"
 
